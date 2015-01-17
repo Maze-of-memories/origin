da20658b623e19d4a1d2905ca86a3420a91d4a2b
Junguru <june0313@gmail.com>
2015-01-17 14:25:57 +0900

    미로 생성 알고리즘 수정

--- MazeGenerator/src/MazeGenerator.java
+++ MazeGenerator/src/MazeGenerator.java
@@ -1,132 +1,111 @@
 import java.util.Random;
 
-
 // 기억의 미로 생성기
 public class MazeGenerator {
-	
-	private static final int NUM_OF_WALLS = 20;
-	
-	private int realSize;
-	private char[][] maze;
-	
-	// 생성자 : 미로를 초기화 한다.
-	public MazeGenerator(int size) {
-		this.realSize = size * 2 -1;
-		maze = new char[realSize][realSize];
-		
-		for(int i = 0; i < maze.length; i++) {
-			for(int j = 0; j < maze[i].length; j++) {
-				maze[i][j] = ' ';
-			}
-		}
-		
-		// 시작지점 표시
-		maze[realSize-1][0] = 'Y';
-		maze[realSize-1][realSize-1] = 'S';
-		
-		// 목표지점 표시
-		maze[0][0] = 'S';
-		maze[0][realSize-1] = 'Y';
-		
-		// 기둥 생성
-		setupCol();
-		
-		// 벽 생성
-		setupWall();
-	}
-	
-	// 기둥을 생성한다.
-	private void setupCol() {
-		for(int i = 1; i < maze.length; i += 2) {
-			for(int j = 1; j < maze[i].length; j += 2) {
-				maze[i][j] = '+';
-			}
-		}
-	}
-	
-	// 랜덤하게 벽을 생성한다.
-	// 벽이 위치할 수 있는 좌표는 (홀수, 짝수) 또는 (짝수, 홀수) 위치이다.
-	private void setupWall() {
-		
-		int walls[] = new int[NUM_OF_WALLS];
-		
-		/*int row;
-		int col;
-		int settedWallCnt = 0;
 
-		Random random = new Random();
+	// 주어진 벽 조합을 이용하여 미로를 생성한다.
+	public Maze generateByWalls(int size, boolean walls[] ) {
 		
-		while(settedWallCnt < NUM_OF_WALLS) {
-			// 벽이 세워질 행을 랜덤하게 선택한다.
-			row = random.nextInt(realSize);
+		int realSize = size * 2 -1;
+		
+		// map 생성 후 초기화 한다.
+		char[][] map = new char[realSize][realSize];
+		initMap(map);
+		
+		int pos = 0;
+		
+		for(int i = 0; i < map.length; i++) {
 			
-			// 행이 짝수인 경우 홀수 열을 랜덤하게 선택한다.
-			if(row % 2 == 0) {
-				col = random.nextInt(realSize/2-1) * 2 + 1;
-				
-				if (maze[row][col] == ' ') {
-					maze[row][col] = '|';
-					settedWallCnt++;
+			if (i % 2 == 0) {
+				// 홀수 행에서는 세로벽 생성
+				for (int j = 1; j < map[i].length; j += 2) {
+					if(walls[pos++])
+						map[i][j] = '|';
+					else
+						map[i][j] = ' ';
 				}
+			} else {
+				// 짝수 행에서는 가로벽 생성
+				for (int j = 0; j < map[i].length; j += 2) {
+					if(walls[pos++])
+						map[i][j] = '-';
+					else
+						map[i][j] = ' ';
+				}
+			}
+		}
+		
+		// 생성된 map으로 Maze를 만든다.
+		Maze maze = new Maze(map);
+		
+		return maze;
+	}
+	
+
+	// 주어진 벽 없이 랜덤으로 미로를 생성하여 리턴한다.
+	// size : 미로의 크기
+	// wallCnt : 생성할 벽의 개수
+	public Maze randomGenerate(int size, int wallCnt) {
+
+		int realSize = size * 2 -1;
+		
+		// map 생성 후 초기화 한다.
+		char[][] map = new char[realSize][realSize];
+		initMap(map);
+		
+		// row and column
+		int r, c;
+		char walChar;
+		Random ran = new Random();
+		
+		// 랜덤하게 벽을 표시하여 map을 생성한다.
+		while (wallCnt != 0) {
+			r = ran.nextInt(realSize);
+			if (r % 2 == 0) {
+				walChar = '-';
+				c = ran.nextInt(size - 1) * 2 + 1;
 			}
 			else {
-				col = random.nextInt(realSize/2) * 2;
-				
-				if (maze[row][col] == ' ') {
-					maze[row][col] = '-';
-					settedWallCnt++;
-				}
-			}			
-		}*/
-		
-		/*for(int i = 0; i < maze.length; i++) {
-			
-			if(i % 2 == 0) {
-				// 세로벽 생성
-				for(int j = 1; j < maze[i].length; j += 2) {
-					maze[i][j] = '|';
-				}
+				walChar = '|';
+				c = ran.nextInt(size) * 2;
 			}
-			else {
-				// 가로벽 생성
-				for(int j = 0; j < maze[i].length; j += 2) {
-					maze[i][j] = '-';
-				}
+
+			if (map[c][r] != walChar) {
+				map[c][r] = walChar;
+				wallCnt--;
 			}
-		}*/
-	}
-	
-	public void wallCombination() {
-		int walls[] = new int[NUM_OF_WALLS];
-		
-		generateCombination(walls, 40, NUM_OF_WALLS);
-	}
-	
-	public void generateCombination(int walls[], int total, int r) {
-		for(int i = 0; i < total; i++) {
-			
-		}
-	}
-		
-	
-	// 미로를 보기 좋게 출력한다.
-	public void printMaze() {
-		
-		for(int i = 0; i < maze.length; i++) {
-			for(int j = 0; j < maze[i].length; j++) {
-				System.out.print(maze[i][j] + " ");
-			}
-			System.out.println("");
-		}
-	}
-	
-	// 미로를 String 타입으로 리턴한다.
-	public String getMaze() {
-		StringBuilder builder = new StringBuilder();
-		for(int i = 0; i < maze.length; i++) {
-			builder.append(maze[i]);
 		}
 		
-		return builder.toString();
+		// 생성된 map으로 Maze를 만든다.
+		Maze maze = new Maze(map);
+		
+		return maze;
+	}
+	
+	// 맵을 초기화 한다.
+	// 길과 기둥을 표시한다.
+	private void initMap(char[][] map) {
+
+		// 길을 표시한다.
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				map[i][j] = ' ';
+			}
+		}
+		
+		// 기둥을 표시한다.
+		for(int i = 1; i < map.length; i += 2) {
+			for(int j = 1; j < map[i].length; j += 2) {
+				map[i][j] = '+';
+			}
+		}
+
+		// 시작지점 표시
+		map[map.length - 1][0] = 'A';
+		map[map.length - 1][map.length - 1] = 'B';
+
+		// 목표지점 표시
+		map[0][map.length - 1] = 'A';
+		map[0][0] = 'B';
 	}
 }
