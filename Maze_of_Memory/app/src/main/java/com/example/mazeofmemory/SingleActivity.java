9b14e09b41be7ebe77bda07a07d904302f90a8f6
Jung SungJin <wjdtjdwlsqkqn@gmail.com>
2015-01-20 06:38:47 +0900

    single_tutorial&icon

--- Maze_of_Memory/app/src/main/java/com/example/mazeofmemory/SingleActivity.java
+++ Maze_of_Memory/app/src/main/java/com/example/mazeofmemory/SingleActivity.java
@@ -2,7 +2,10 @@ package com.example.mazeofmemory;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class SingleActivity extends Activity {
@@ -12,6 +15,15 @@ public class SingleActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single);
 
+        Button btn;
+
+        btn = (Button)findViewById(R.id.single_tutorial);
+        btn.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(SingleActivity.this, SingleTutorialActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
---
+++ Maze_of_Memory/app/src/main/java/com/example/mazeofmemory/SingleTutorialActivity.java
@@ -0,0 +1,18 @@
+package com.example.mazeofmemory;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SingleTutorialActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_tutorial);
+
+
+    }
+}
