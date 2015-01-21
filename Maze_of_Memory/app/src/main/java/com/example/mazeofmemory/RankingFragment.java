a05ea148e10c50c912c3a5a030ce9147d9beee3d
Junguru <june0313@gmail.com>
2015-01-21 18:40:31 +0900

    랭킹 액티비티 추가

---
+++ Maze_of_Memory/app/src/main/java/com/example/mazeofmemory/RankingFragment.java
@@ -0,0 +1,136 @@
+package com.example.mazeofmemory;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link RankingFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class RankingFragment extends Fragment {
+
+    private static final String TAG = "RankingFragment";
+
+    private OnFragmentInteractionListener mListener;
+
+    // 세션 변경 이벤트 리스너
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            onSessionStateChange(session, state, exception);
+        }
+    };
+    private UiLifecycleHelper uiHelper;
+
+    public RankingFragment() {
+        // Required empty public constructor
+    }
+
+    // 세션 상태 변경시 호출되는 메서드
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        if (state.isOpened()) {
+            // 계정 인증 후의 화면을 보여준다.
+            Log.i(TAG, "Logged in...");
+        } else if (state.isClosed()) {
+            // 계정이 인증되지 않았을 때의 화면을 보여준다.
+            Log.i(TAG, "Logged out...");
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // uiHelper 초기화
+        uiHelper = new UiLifecycleHelper(getActivity(), callback);
+        uiHelper.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // 프래그먼트의 레이아웃을 생성하고 리턴한다.
+        View view = inflater.inflate(R.layout.activity_ranking, container, false);
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        uiHelper.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
--- Maze_of_Memory/app/src/main/java/com/example/mazeofmemory/SingleActivity.java
+++ Maze_of_Memory/app/src/main/java/com/example/mazeofmemory/SingleActivity.java
@@ -15,8 +15,8 @@ public class SingleActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single);
 
+        // 튜토리얼 버튼
         Button btn;
-
         btn = (Button)findViewById(R.id.single_tutorial);
         btn.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
@@ -24,6 +24,17 @@ public class SingleActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+        // 랭킹보기 버튼
+        Button btnShowRank = (Button)findViewById(R.id.btnShowRank);
+        btnShowRank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SingleActivity.this, RankingActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 
